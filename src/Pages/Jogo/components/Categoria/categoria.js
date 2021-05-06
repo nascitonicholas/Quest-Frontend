fbcb0c134352929dec44262078303321e6683400
nascito_nicholas <nascito.nicholas@gmail.com>
2021-05-05 21:47:43 -0300

    Criando componente comum, quadrados.

--- src/Pages/Jogo/components/Categoria/categoria.js
+++ src/Pages/Jogo/components/Categoria/categoria.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Quadrados from '../../../../shared/components/Quadrados/quadrados.js';
 
 export default function Categoria() {
-  const [categorias] = localStorage.getItem('categoriasPerguntaRodada') || ['Tecnologia','Português'];
+  const categorias = localStorage.getItem('categoriasPerguntaRodada') || ['Tecnologia','Português', 'Hist', 'Mate'];
 
-  return(<Quadrados itens={categorias} page={'categorias'} />);
+  return(<Quadrados itens={categorias} page='Categorias' />);
 };
--- src/shared/components/Quadrados/quadrados.js
+++ src/shared/components/Quadrados/quadrados.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import './quadrados.css'
 
-export default function criaQuadrados( {itens, page }) {
+export default function CriaQuadrados({itens, page}) {
 
-  return(
-    <div>
-      {
-        itens.map(iten => (
-          <div>
-            <div/>
-            <div>
-              <p>{iten}</p>
+  switch (page) {
+    case 'Categorias':
+      return(
+        <div className="quadrado-container">
+          <ul>
+            {itens.map(item => (
+              <li>
+                <p>{item}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    default:
+      return(
+        <div className="quadrado-container">
+          {itens.map(item => (
+            <div className="item-container">
+              <div>
+                <p>{item}</p>
+              </div>
             </div>
-          </div>
-        ))
-      }
-    </div>
-  );
-};
+          ))}
+        </div>
+      );
+  };
+}
