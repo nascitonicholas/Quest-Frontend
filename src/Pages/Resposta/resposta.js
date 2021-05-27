c783c803f252a5334cf9be3e4a488d848b6cc0f0
nascito_nicholas <nascito.nicholas@gmail.com>
2021-05-27 18:28:04 -0300

    Criação do cronometro e ajuste da tela de resposta.

--- src/Pages/Resposta/resposta.js
+++ src/Pages/Resposta/resposta.js
@@ -1,29 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import Header from '../../shared/components/Header/header.js';
 import Footer from '../../shared/components/Footer/footer.js';
+import Timer from '../../shared/components/Cronometro/timer.jsx';
 import './resposta.css';
 
 export default function Resposta() {
   const perguntaEscolhida = localStorage.getItem('perguntaEscolhida');
-  const alternativas = localStorage.getItem('alternativasDisponiveis');
+  const alternativas = localStorage.getItem('alternativasDisponiveis').split(',');
 
   return (
     <div>
       <Header />
       <h1>{perguntaEscolhida.toUpperCase()}</h1>
-      <div className="quadrado-container">
-          <ul>
-            {alternativas.map(alternativa => (
-              <li key={alternativa}>
-                <div>
-                  <button type="button"></button>
-                </div>
-                <p>{alternativa}</p>
-              </li>
-            ))}
-          </ul>
+      <div className="cronometro">
+        <Timer />
+      </div>
+      <div className="alternativas-container">
+        <div className="blocos-container">
+         <p>{alternativas[0]}</p>
+         <p>{alternativas[1]}</p>
         </div>
+        <div className="blocos-container">
+         <p>{alternativas[2]}</p>
+         <p>{alternativas[3]}</p>
+        </div>
+      </div>
       <Footer />
     </div>
   );
